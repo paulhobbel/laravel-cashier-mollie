d8c2ac2b5037ba0df69f6d72bb67ea1c1644c523
Dizzy <itsdizzy40@gmail.com>
2017-03-01 23:18:04 +0100

    Added ServiceProvider and Cashier base class

---
+++ CashierServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dizzy\Cashier;
+
+use Illuminate\Support\ServiceProvider;
+
+class CashierServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}

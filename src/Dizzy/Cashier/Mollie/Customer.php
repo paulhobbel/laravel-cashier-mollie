154686f4b8aa95b23e1f2ac7da1c9a9449256d37
Dizzy <itsdizzy40@gmail.com>
2017-03-02 23:43:30 +0100

    Removed PhpStorm autogenerated headers

--- src/Dizzy/Cashier/Mollie/Customer.php
+++ src/Dizzy/Cashier/Mollie/Customer.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: paulh
- * Date: 2-3-2017
- * Time: 23:12
- */
 
 namespace Dizzy\Cashier\Mollie;
 
--- src/Dizzy/Cashier/Mollie/Mollie.php
+++ src/Dizzy/Cashier/Mollie/Mollie.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: paulh
- * Date: 2-3-2017
- * Time: 15:35
- */
 
 namespace Dizzy\Cashier\Mollie;
 
